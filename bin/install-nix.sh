b47ad56c388e86429697de6037fcca761357f91a
Jesse Pledger <jesse@futurefoundry.co>
2021-04-25 20:58:54 -0700

    Add TODOs to bin/install-nix.sh

--- bin/install-nix.sh
+++ bin/install-nix.sh
@@ -7,4 +7,6 @@ URL="https://github.com/numtide/nix-flakes-installer/releases/download/nix-2.4pr
 [[ $(uname -s) = "Darwin" ]] && FLAG="--darwin-use-unencrypted-nix-store-volume"
 [[ ! -z "$1" ]] && URL="$1"
 
+# TODO Create ~/.local/share/nix/trusted-settings.json
+
 command -v nix > /dev/null && echo "nix is already installed on this system." || bash <(curl -L $URL) --daemon $FLAG
