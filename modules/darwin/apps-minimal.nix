115e3596bf0879db7f5af3edd4c49e35cd1bc4e0
Jesse Pledger <jessepledger@gmail.com>
2021-06-20 12:33:15 -0700

    Update apps-minimal.nix

--- modules/darwin/apps-minimal.nix
+++ modules/darwin/apps-minimal.nix
@@ -1,12 +1,8 @@
 { config, lib, pkgs, ... }: {
   homebrew = {
     casks = [
-      "1password"
-      "firefox-beta"
       "karabiner-elements"
-      "keepingyouawake"
       "kitty"
-      "raycast"
       "visual-studio-code"
     ];
   };
