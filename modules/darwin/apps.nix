393dc6185f3e8c35ea66bcbe5e283e1efb008d94
Jesse Pledger <jessepledger@gmail.com>
2021-06-22 11:15:38 -0700

    Reduce apps in apps.nix

--- modules/darwin/apps.nix
+++ modules/darwin/apps.nix
@@ -1,23 +1,18 @@
 { config, lib, pkgs, ... }: {
   homebrew = {
     casks = [
-      "ableton-live-suite10"
-      "amethyst"
       "backblaze"
       "brave-browser"
       "bettertouchtool"
       "cleanmymac3"
-      "cycling74-max"
       "docker"
       "dropbox"
-      "eul"
       "evernote"
       "fantastical"
       "fork"
       "gpg-suite"
       "gswitch"
       "iina"
-      "intellij-idea"
       "istat-menus"
       "iterm2"
       "karabiner-elements"
@@ -31,11 +26,8 @@
       "signal"
       "slack"
       "syncthing"
-      "timely"
-      "transmission"
       "virtualbox"
       "visual-studio-code"
-      "vlc"
       "zoom"
     ];
     masApps = { "Unsplash Wallpapers" = 1284863847; };
